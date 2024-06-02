742d524cbcfa93f4d5b3a045ef0479c51b759936
George Koridze <150628670+gi0rgi-k@users.noreply.github.com>
2024-06-02 19:49:35 +0200

    Create Tutorial_Lab_3_Static_Members.py

---
+++ Unit02/Codio/Tutorial_Lab_3_Static_Members.py
@@ -0,0 +1,27 @@
+"""
+Tutorial Lab 3: Static Members
+"""
+
+class Person:
+    count = 0
+    
+    """Represents a generic Person."""
+    
+    def __init__(self, first, last, weight, height):
+        self.first_name = first
+        self.last_name = last
+        self.weight_in_lbs = weight
+        self.height_in_inches = height
+        Person.count = Person.count + 1
+
+    def calc_bmi(self):
+        
+        return (self.weight_in_lbs * 703) / self.height_in_inches ** 2
+
+
+p = Person('Tom', 'Thumb', 150, 62)
+p2 = Person('Fred', 'Flint', 225, 57)
+
+print(p.calc_bmi())
+print(p2.calc_bmi())
+print(Person.count)
