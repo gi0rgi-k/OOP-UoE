c1e1af756f90b3720eb16c8a910894c89c37d471
George Koridze <150628670+gi0rgi-k@users.noreply.github.com>
2024-06-02 10:44:39 +0200

    Update Challenge.py

--- Unit01/Codio/Challenge.py
+++ Unit01/Codio/Challenge.py
@@ -7,17 +7,17 @@ class Person:
         self.weight_in_lbs = weight
         self.height_in_inches = height
 
-# Create five Person objects with appropriate values
-p1 = Person("Tom", "Smith", 180, 70)
-p2 = Person("Fred", "Johnson", 200, 72)
+# Create five different Person objects with appropriate values
+p1 = Person("Tom", "Keshelava", 180, 70)
+p2 = Person("Fred", "Koridze", 200, 72)
 p3 = Person("George", "Williams", 150, 68)
 p4 = Person("Tanya", "Brown", 130, 65)
-p5 = Person("Mary", "Jones", 140, 64)
+p5 = Person("Mary", "Jameson", 140, 64)
 
-# Create a list to store the Person objects
+# Create a list to store the above Person objects
 person_list = [p1, p2, p3, p4, p5]
 
-# Use a for loop to iterate over the list and print out the first names
+# Use a 'for' loop to iterate over the list and print out the first names
 for person in person_list:
     print(person.first_name)
 
