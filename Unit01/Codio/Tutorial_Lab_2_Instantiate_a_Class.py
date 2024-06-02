9bfd1a83be4d70922816eb99c065d3b1164f6143
George Koridze <150628670+gi0rgi-k@users.noreply.github.com>
2024-06-02 03:38:10 +0200

    Update Tutorial_Lab_2_Instantiate_a_Class.py

--- Unit01/Codio/Tutorial_Lab_2_Instantiate_a_Class.py
+++ Unit01/Codio/Tutorial_Lab_2_Instantiate_a_Class.py
@@ -1,1 +1,11 @@
+class Person:
+    """Represents a generic Person."""
+    def __init__(self, first, last, weight, height):
+          self.first_name = first
+          self.last_name = last
+          self.weight_in_lbs = weight
+          self.height_in_inches = height
 
+p = Person('Tom', 'Thumb', 150, 78)
+
+print(p.first_name + ' ' + p.last_name + ' weighs ' + str(p.weight_in_lbs) + 'lbs.')
